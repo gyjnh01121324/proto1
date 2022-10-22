0ad48835d3b909dea70b5f76a7a1a3279e5d27a6
gyjnh01121324 <mr.smeloff12@yandex.ru>
2022-10-22 03:52:23 +0300

    message

--- main.go
+++ main.go
@@ -8,17 +8,38 @@ import (
 
 )
 
+type User struct {
+  Name string `json"name"`
+  Age uint16 `json:"age"`
+}
+
 func main () {
-db, err := sql.Open("mysql", "root:roo@tcp(127.0.0.1:3306)/golang")
+db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
 if err != nil {
   panic(err)
 }
+
 defer db.Close ()
-//Установка данных
-insert, err := db.Query("INSERT INTO `users` (`name`, `age` ) VALUES('Alex', 25)")
+// Установка данных
+// insert, err := db.Query("INSERT INTO `users` (`name`, `age` ) VALUES('Bob', 35)")
+// if err != nil {
+//   panic(err)
+// }
+// defer insert.Close()
+// Выборка данных
+res, err := db.Query("SELECT `name`, `age` FROM `users`")
 if err != nil {
   panic(err)
 }
-defer insert.Close()
- fmt.Println("Подключение к MySQL")
+
+for res.Next() {
+  var user User
+  err = res.Scan(&user.Name, &user.Age)
+  if err != nil {
+    panic(err)
+  }
+
+   fmt.Println(fmt.Sprintf("User %s with age %d", user.Name, user.Age))
+}
+
 }
